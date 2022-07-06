13c961276313cc7fa18e419f09ccc0ffb14cc056
David Hatfield <davidhatfield@gmjr@gmail.com>
2022-07-06 12:21:24 -0400

    Proudhon shakes his damn head

---
+++ TenmoServer/Controllers/Account.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TenmoServer.Models;
+
+namespace TenmoServer.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class Account : ControllerBase
+    {
+        private readonly string SQLBalanceString = "select balance from users inner join accounts on accounts.user_id = users.user_id where users.username = @name;";
+
+
+        public Account()
+        {
+            // to do dao stuff and things 
+        }
+
+        [HttpGet ("")] // may need to change inside ""
+        public AccountBalance GetBalance(string username)
+        {   
+            AccountBalance accountBalance = 
+            return 
+        }
+
+    
+}
